5fdb64e82b9bf797d9e29950f265695ec045a942
Cristobal Fresno <cfresno@inmegen.gob.mx>
2017-05-06 17:57:09 -0500

    Split plot functions.

--- R/per_base_sequence_quality.R
+++ R/per_base_sequence_quality.R
@@ -13,24 +13,24 @@ library("splines")
 ################################################################################
 ##-Per_base_sequence_quality
 ################################################################################
-readPBSQ<-function(filePBSQ){
-    pbsq<-lapply(filePBSQ, read.table, sep="\t", comment.char ="*", 
+readPBSQ <- function(filePBSQ){
+    pbsq <- lapply(filePBSQ, read.table, sep="\t", comment.char ="*",
         header=TRUE)
-    pbsq<-do.call(rbind,lapply(1:length(filePBSQ), function(x){
-        pbsq[[x]]$File<-filePBSQ[x]
+    pbsq <- do.call(rbind,lapply(1:length(filePBSQ), function(x){
+        pbsq[[x]]$File <- filePBSQ[x]
         return(pbsq[[x]])
     }))
-    names(pbsq)<-c("Base", "Mean", "Q2", "Q1", "Q3", "P10", "P90", "File")
+    names(pbsq) <- c("Base", "Mean", "Q2", "Q1", "Q3", "P10", "P90", "File")
 
     ##Change the name to include additional data.
-    aux<-as.data.frame(do.call(rbind, strsplit(gsub(pbsq$File, 
+    aux <- as.data.frame(do.call(rbind, strsplit(gsub(pbsq$File,
         pattern="_fastqc_data.txt.Per_base_sequence_quality", replacement=""),
         split="_")))
-    names(aux)<-c("Subject", "NA1", "Flowcell", "Lane", "PairEnd")
-    pbsq$File<-NULL
-    pbsq<-cbind(pbsq, aux)
+    names(aux) <- c("Subject", "NA1", "Flowcell", "Lane", "PairEnd")
+    pbsq$File <- NULL
+    pbsq <- cbind(pbsq, aux)
     ##Order the Base levels
-    pbsq$Base<-factor(as.character(pbsq$Base), 
+    pbsq$Base <- factor(as.character(pbsq$Base),
             levels=levels(pbsq$Base)[
                 order(as.integer(unlist(lapply(strsplit(
                     levels(pbsq$Base), split="-"), function(x)x[1]))))])
@@ -38,77 +38,37 @@ readPBSQ<-function(filePBSQ){
 }
 
 ##Single per base sequence quality plot#########################################
-singlePerBaseQuality<- function(datum){
-    datum$Base<-factor(as.character(datum$Base), 
+singlePerBaseQuality <-  function(datum){
+    datum$Base <- factor(as.character(datum$Base),
         levels=levels(datum$Base)[
             order(as.integer(unlist(lapply(strsplit(
                 levels(datum$Base), split="-"), function(x)x[1]))))])
-    datum$x<-1:nrow(datum)    
-    xMax<-length(levels(datum$Base))
-    background<-data.frame(
+    datum$x <- 1:nrow(datum)
+    xMax <- length(levels(datum$Base))
+    background <- data.frame(
                     x=rep(c(0,xMax,xMax,0),3),
                     y=c(c(0,0,20,20),c(20,20,28,28),c(28,28,42,42)),
                     Quality=c(rep("Bad", 4),rep("Intermediate", 4),rep("Good", 4)))
-    sp<-ggplot()+
+    sp <- ggplot()+
         geom_polygon(data=background, aes(x=x, y=y, group=Quality, fill=Quality))+
-        geom_boxplot(data=datum, aes(x=x, ymin=P10, lower=Q1, middle=Q2, 
+        geom_boxplot(data=datum, aes(x=x, ymin=P10, lower=Q1, middle=Q2,
             upper=Q3, ymax=P90, group=Base), stat = "identity", fill="yellow")+
         geom_line(data=datum, aes(x=x, y=Mean, group=Subject), color="blue")+
         xlab("Position in read (bp)")+
         scale_x_continuous(breaks=datum$x, labels=datum$Base)+
         theme(axis.text.x=element_text(angle=45, hjust=1),
-            legend.position="bottom")       
+            legend.position="bottom")
     return(sp)
 }
-  
+
 
 ##Smooth per base sequence quality plot#########################################
-#Transparency work around 
+#Transparency work around
 # http://tinyheero.github.io/2015/09/15/semi-transparency-r.html
 #in ~./Rprofile
 #setHook(packageEvent("grDevices", "onLoad"),
 #function(...) grDevices::X11.options(type='cairo'))
 #options(device='x11')
-
-data<-pbsq
-#smoothPerBaseQuality<- function(data){
-    data
-    type<-"Mean"
-    data$x<-as.integer(unclass(data$Base))
-    xMax<-length(levels(data$Base))
-    background<-data.frame(
-                    x=rep(c(0,xMax,xMax,0),3),
-                    y=c(c(0,0,20,20),c(20,20,28,28),c(28,28,42,42)),
-                    Quality=c(rep("Bad", 4),rep("Intermediate", 4),rep("Good", 4)))
-    tapply(data$P10, INDEX=data$x,sd)                
-                
-    sp<-ggplot()+
-        geom_hline(aes(yintercept=20), linetype="dashed", color="red", alpha=0.5)+
-        geom_hline(aes(yintercept=28), linetype="dashed", color="green", alpha=0.5)+
-        geom_jitter(data=data, aes(x=x, y=P90), alpha=0.03)+
-        #geom_smooth(data=data, aes(x=x, y=P90), se=FALSE)+
-        geom_jitter(data=data, aes(x=x, y=P10), alpha=0.03)+
-        #geom_smooth(data=data, aes(x=x, y=P10), se=FALSE)+
-        geom_jitter(data=data, aes(x=x, y=Q1), color="green", alpha=0.03)+
-        #geom_smooth(data=data, aes(x=x, y=Q1), color="green", se=FALSE)+
-        geom_jitter(data=data, aes(x=x, y=Q3), color="green", alpha=0.03)+
-        #geom_smooth(data=data, aes(x=x, y=Q3), color="green", se=FALSE)+
-        geom_jitter(data=data, aes(x=x, y=Q2), color="brown", alpha=0.03)+
-        geom_smooth(data=data, aes(x=x, y=Q2), color="brown", se=FALSE)+
-        geom_jitter(data=data, aes(x=x, y=Mean), color="purple", alpha=0.03)+
-        geom_smooth(data=data, aes(x=x, y=Mean), color="purple", se=FALSE)+
-        xlab("Position in read (bp)")+
-        scale_x_continuous(breaks=data$x, labels=data$Base)+
-        theme(axis.text.x=element_text(angle=45, hjust=1))
-        ##FIXME: add color legend!!!
-    sp     
-        
-    ##Region plot
-    ##R1: min(P10) - max(P90)
-    r1<-data.frame(
-        x=c(1:xMax, xMax:1),
-        y=c(tapply(data$P10, INDEX=data$x, min),
-            tapply(data$P90, INDEX=data$x, max)[xMax:1]))
 
 smooth_polygon_edge <- function(coordinates) {
     smooth_model <- loess(y ~ x, data=coordinates)
@@ -159,10 +119,46 @@ plot_trend_line <- function(coordinates, color, plot=ggplot()) {
 
 ## FIXME Add legends and axis labels
 read_data <- function(data_dir) {
-	filePBSQ<-list.files(path=data_dir, pattern="Per_base_sequence_quality")
+	filePBSQ <- list.files(path=data_dir, pattern="Per_base_sequence_quality")
 	# per base sequence quality
-	pbsq<-readPBSQ(filePBSQ)
+	pbsq <- readPBSQ(filePBSQ)
+	pbsq$x <- as.integer(unclass(pbsq$Base))
 	return(pbsq)
+}
+
+plot_labels <-function(plot, x_label, y_label) {
+	plot <- plot + xlab(x_label)
+	plot <- plot + ylab(y_label)
+	return(plot)
+}
+
+plot_legend <-function(plot) {
+	out_color="#eded44"
+	middle_color="#01cbf3"
+	in_color="#43640b"
+	mean_color="#fdd65d"
+        median_color="#f6a801"
+
+	labels <- data.frame(Regions=c(out_color, middle_color, in_color), labels=c("P10-P90", "Q1-Q3", "Mean-Median"))
+	values <- labels$Regions; names(values)=labels$labels
+	glegend <- ggplot(data=labels, aes(x=1, y=1, fill=Regions, label=labels))+
+		geom_tile() +
+		scale_fill_manual(name="Regions", values=values)
+	glegend
+
+	plot <- plot + scale_x_continuous(breaks=x_breaks, labels=x_labels)
+	y_breaks <- seq(from=0, to=42, by=2)
+	plot <- plot + ylim(0, 42) + scale_y_continuous(breaks=y_breaks, labels=as.character(y_breaks))
+	return(plot)
+}
+
+plot_axis <-function(plot, data) {
+	x_breaks <- c(1:9, seq(from=11, to=38, by=3))
+	x_labels <- levels(data$Base)[x_breaks]
+	plot <- plot + scale_x_continuous(breaks=x_breaks, labels=x_labels)
+	y_breaks <- seq(from=0, to=42, by=2)
+	plot <- plot + ylim(0, 42) + scale_y_continuous(breaks=y_breaks, labels=as.character(y_breaks))
+	return(plot)
 }
 
 main <- function() {
@@ -180,10 +176,13 @@ main <- function() {
 	spr <- plot_quality_limits(spr)
 	spr <- plot_trend_line(coordinates=data.frame(x=data$x, y=data$Q2), median_color, spr)
 	spr <- plot_trend_line(coordinates=data.frame(x=data$x, y=data$Mean), mean_color, spr)
+	spr <- plot_labels(spr, "Position in read (bp)", "Quality (10 ⨉ -log(pe))")
+	spr <- plot_axis(spr, data)
 	spr
+
 }
 
 test <- function() {
-	datum<-subset(pbsq, Subject=="SM-3MG3L" & Lane=="L1" & PairEnd=="1")
+	datum <- subset(pbsq, Subject=="SM-3MG3L" & Lane=="L1" & PairEnd=="1")
 	singlePerBaseQuality(datum)
 }
