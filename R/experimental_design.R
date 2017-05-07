77f03382e404910bb556bbf528529f4abe2196e9
Cristobal Fresno <cfresno@inmegen.gob.mx>
2017-05-06 20:14:45 -0500

    Plot design qith tiles

--- R/experimental_design.R
+++ R/experimental_design.R
@@ -3,15 +3,14 @@ library("reshape2")
 library("cowplot")
 
 read_data <- function(file_name="summary.txt"){
-	fastq <- read.table(file_name, header=TRUE)
-	names(fastq)[1]<-"reads"
-	fastq$filename <- gsub(fastq$filename, pattern=".fq.gz", replacement="")
-	fastq$filename <- gsub(fastq$filename, pattern="data/", replacement="")
-	design <- do.call(rbind, strsplit(gsub(fastq$filename,
+	fastq <- read.table(file_name, header=FALSE)
+	names(fastq)[1:8]<-c("Reads", "FileName", "Sequencer", "Run", "Flowcel", "Pair", "N", "Bad")
+	fastq$filename <- gsub(fastq$FileName, pattern=".fq.gz", replacement="")
+	fastq$filename <- gsub(fastq$FileName, pattern="data/", replacement="")
+	design <- do.call(rbind, strsplit(gsub(fastq$FileName,
 		pattern=".fq.gz", replacement=""), split="_"))
 	colnames(design)<-c("Subject", "NA1", "Flowcell", "Lane", "PairEnd")
 	fastq <- cbind(fastq, design)
-	names(fastq)[1:8]<-c("Reads", "FileName", "Sequencer", "Run", "Flowcel", "Pair", "N", "Bad")
 	return(fastq)
 }
 
@@ -23,12 +22,16 @@ check_integrity <- function(fastq){
 	return(fastq)
 }
 
+plot_sequencing <- function(fastq){
+	plot_design <- ggplot(data=fastq, aes(x=Lane, y=Pair, fill=Subject)) +
+		geom_tile() + facet_grid(Run ~ Sequencer)
+	plot_design
+	return(plot_design)
+}
+
 fastq <- read_data()
 fastq <- check_integrity(fastq)
-
-plot_design <- ggplot(data=fastq, aes(x=Lane, y=PairEnd, fill=Subject)) +
-	geom_tile() + facet_grid(Sequencer ~ Run)
-plot_design
+plot_sequencing(fastq)
 
 
 
