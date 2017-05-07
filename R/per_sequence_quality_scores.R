febc5d5d10f88be46422e29f91310778a961ace2
Cristobal Fresno <cfresno@inmegen.gob.mx>
2017-05-07 14:23:21 -0500

    Per sequence quality score added

---
+++ R/per_sequence_quality_scores.R
@@ -0,0 +1,37 @@
+options(width=120)
+library("ggplot2")
+library("reshape2")
+library("cowplot")
+library("splines")
+library("plyr")
+################################################################################
+readPSQS <- function(data_dir, group="Raw"){
+	files <- list.files(path=data_dir, pattern="Per_sequence_quality_scores", full.names=TRUE)
+	data <- lapply(files, read.table, sep="\t", comment.char ="*", header=TRUE)
+	data <- lapply(data,function(x){
+        aux <- t(x)
+        colnames(aux)<-aux[1,]
+        return(aux[-1, , drop=FALSE])
+	})
+    data <- do.call(rbind.fill,lapply(data, as.data.frame))
+    data <- colSums(data, na.rm=TRUE)
+    return(data.frame(Quality=names(data), Count=data, Group=group))
+}
+
+plot_mean_quality_density<-function(data){
+    data$Quality <- as.integer(as.character(data$Quality))
+    plot_obj <- ggplot(
+            data=data, 
+            aes(x=Quality, y=Count/10^6, group=Group, color=Group)) +
+        geom_line()+ 
+        #geom_smooth(se=FALSE)+
+        xlab("Mean Sequence Quality (Phred Score)") +
+        ylab("Number of reads [x 10^6]")+
+        theme(legend.position="bottom")
+    return(plot_obj)
+}
+
+data_dir<-"/home/cfresno/ssh/castillo/tmp/summary/data"
+data <- readPSQS(data_dir, group="Raw")
+plot_mean_quality_density(data)
+
