a4823cbc854756b0d963e80e7fc8ec5368568105
Cristobal Fresno <cfresno@inmegen.gob.mx>
2017-05-07 19:42:06 -0500

    Experimental design new format

--- R/experimental_design.R
+++ R/experimental_design.R
@@ -4,25 +4,27 @@ library("cowplot")
 
 read_data <- function(file_name="experiment.stats"){
 	fastq <- read.table(file_name, header=TRUE)
-	names(fastq)[1:8]<-c("Reads", "FileName", "Sequencer", "Run", "Flowcel", "Pair", "Bad", "Control")
-	fastq$filename <- gsub(fastq$FileName, pattern=".fq.gz", replacement="")
-	fastq$filename <- gsub(fastq$FileName, pattern="data/", replacement="")
+	names(fastq)<-sapply(names(fastq), function(x){
+		paste(toupper(substr(x,1,1)), 
+			substr(x, 2, nchar(x)), sep="")
+	})
+	fastq$Filename <- gsub(fastq$Filename, pattern=".fq.gz", replacement="")
+	fastq$Filename <- gsub(fastq$Filename, pattern="data/", replacement="")
 	fastq$Run <- factor(fastq$Run)
-	design <- do.call(rbind, strsplit(gsub(fastq$FileName,
+	design <- do.call(rbind, strsplit(gsub(fastq$Filename,
 		pattern=".fq.gz", replacement=""), split="_"))
-	colnames(design)<-c("Subject", "NA1", "Flowcell", "Lane", "PairEnd")
+	colnames(design)<-c("Subject", "NA1", "Flowcell", "Lanes", "PairEnd")
 	fastq <- cbind(fastq, design)
 	fastq$Subject <- factor(as.character(fastq$Subject), levels = sample(levels(fastq$Subject)))
-	fastq$Flowcell <- factor(as.character(fastq$Flowcell), levels = sample(levels(fastq$Flowcell)))
-	fastq$Lane <- as.integer(gsub(fastq$Lane, pattern="L", replacement=""))
+	fastq$Flowcel <- factor(as.character(fastq$Flowcel), levels = sample(levels(fastq$Flowcel)))
+	fastq$Lanes <- gsub(fastq$Lanes, pattern="L", replacement="")
 	return(fastq)
 }
 
 check_integrity <- function(fastq){
 	stopifnot(all(fastq$Flowcel == fastq$Flowcell)); fastq$Flowcel <- NULL
 	stopifnot(all(fastq$Pair == fastq$PairEnd)); fastq$PairEnd <- NULL
-	stopifnot(all(fastq$Control == 0)); fastq$Control <- NULL
-	stopifnot(all(fastq$Bad == "N")); fastq$Bad <- NULL
+	stopifnot(all(fastq$Lane == fastq$Lanes)); fastq$Lanes <- NULL
 	return(fastq)
 }
 
@@ -48,15 +50,15 @@ plot_reads_per_subject <- function(fastq){
 }
 
 fastq <- read_data()
-fastq <- check_integrity(fastq)
+#fastq <- check_integrity(fastq)
 #plot_sequencing(fastq)
-plot_reads_per_subject(fastq)
+#plot_reads_per_subject(fastq)
 
 #How many Flowcell were used by each subject?
-table(rowSums(with(unique(fastq[, c("Subject", "Flowcell")]), table(Subject, Flowcell))))
+#table(rowSums(with(unique(fastq[, c("Subject", "Flowcell")]), table(Subject, Flowcell))))
 # 1  2  3 
 #63 19  4
 #Are there sequencer effect? 
-table(rowSums(with(unique(fastq[, c("Subject", "Sequencer")]), table(Subject, Sequencer))))
+#table(rowSums(with(unique(fastq[, c("Subject", "Sequencer")]), table(Subject, Sequencer))))
 # 1  2  3 
 #65 19  2 
