1161a751f3570541f897ec2f2d7839e3fcadf9c8
Cristobal Fresno <cfresno@inmegen.gob.mx>
2017-05-07 21:38:37 -0500

    Working sequence reads.

--- R/fastq_reads.R
+++ R/fastq_reads.R
@@ -1,7 +1,6 @@
 rm(list=ls())
-load("/home/cfresno/Dropbox/inmegen/100g/fq/R/fastqc_data.RData")
-load(file="/home/cfresno/Dropbox/inmegen/100g/fq/R/Reads.RData")
-rm(list=ls()[!ls()%in%c("raw_data", "bgi_data", "inmegen_data")])
+load("/home/cfresno/Dropbox/inmegen/100g/fq/R/Reads.RData")
+rm(list=ls()[!ls()%in%c("raw_psqs", "bgi_psqs", "inmegen_psqs")])
 
 source("per_base_sequence_quality.R")
 source("per_sequence_quality_scores.R")
@@ -12,12 +11,10 @@ library("cowplot")
 
 
 ##Read comparison
-raw_psqs <- readPSQS("data/raw", group="Raw")
-bgi_psqs <- readPSQS("data/clean", group="BGI")
-inmegen_psqs <- readPSQS("data/trimmomatic", group="INMEGEN")
-#save.image(file="results/Reads.RData", compress="xz")
+# raw_psqs <- readPSQS("data/raw", group="Raw")
+# bgi_psqs <- readPSQS("data/clean", group="BGI")
+# inmegen_psqs <- readPSQS("data/trimmomatic", group="INMEGEN")
+# save.image(file="results/Reads.RData", compress="xz")
 
-#mean_quality <- plot_mean_quality_density(data)
-
-#save.image(file="results/Comparison.RData", compress="xz")
+plot_mean_quality_density(rbind(raw_psqs, bgi_psqs, inmegen_psqs))
 
